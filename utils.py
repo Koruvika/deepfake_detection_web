4b05cca467dc19798cc60bc3628c269346e4c295
khuongle25 <85564329+khuongle25@users.noreply.github.com>
2023-11-03 04:19:14 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import boto3
 
 # Configure AWS credentials
 aws_access_key_id = 'AKIA2PZGTZ4CWGXVER4J'
-aws_secret_access_key = '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'
 
 # Configure the S3 client
 s3_client = boto3.client(
